c9007b32782820a3c1973ed8c9b54eb4346537de
Alex Kazorian <akazorian@berkeley.edu>
2017-11-24 15:05:25 -0800

    Added a script to build the website based on flags passed in. Moved in CS61B notes. Built website. Also a test:

--- gen_file.py
+++ gen_file.py
@@ -7,11 +7,12 @@ import re
 relative_path = os.getcwd()
 nav = '<li><a href="#{}">{}</a></li>\n'
 
-def gen_file(File=None, FULLPATH=None, O=None):
+def gen_file(File=None, FULLPATH=None, O=None, link=None):
     if FULLPATH != None:
         gen_helper(FULLPATH)
     else:
         gen_helper(File)
+        gen_link(File, link)
 
 def gen_helper(File):
     with open("temp.html", 'w+') as html:
@@ -55,7 +56,7 @@ def gen_nav(File, ids, headers, depth, lines):
         for line in lines:
             html.write(line)
             if pattern.match(line):
-                html.write('<div id="mySidenav" class="sidenav">\n<nav>\n<ul>\n')
+                html.write('<div id="mySidenav" class="sidenav">\n<ul>\n')
                 curr_depth = depth[0]
                 for i in range(len(ids)):
                     temp_depth = depth[i]
@@ -66,12 +67,18 @@ def gen_nav(File, ids, headers, depth, lines):
                             html.write('</ul>\n')
                     curr_depth = temp_depth
                     html.write(nav.format(ids[i], headers[i]))
-                html.write('</ul>\n</nav>\n</div>\n')
+                html.write('</ul>\n</div>\n')
+
+def gen_link(File, link):
+    print(File, link)
+    with open("index.html", 'a') as home:
+        home.write("<a href={}> {}</a>\n".format(File.replace(".tex", ".html"), link))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate a webpage for a LaTex file.")
+    parser.add_argument('link', help="Name of link to generated file.")
     parser.add_argument('--file', help="Name of TeX file in relative path for which a webpage will be produced.")
     parser.add_argument('--fullpath', help="If provided, then file at the provided path will be used to generate a webpage.")
     parser.add_argument('--o', help="Optional name of output file.")
     args = parser.parse_args()
-    gen_file(File=args.file, FULLPATH=args.fullpath, O=args.o)
+    gen_file(File=args.file, FULLPATH=args.fullpath, O=args.o, link=args.link)
