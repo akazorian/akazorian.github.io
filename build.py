d5e36efc8be55a32187e7fb085d6d7505a3d42bd
Alex Kazorian <akazorian@berkeley.edu>
2018-01-08 22:14:30 -0800

    Making some progress

--- build.py
+++ build.py
@@ -5,15 +5,6 @@ import os
 import re
 from gen_file import *
 
-topic_map = {}
-pattern = re.compile(".tex")
-for dirpath, dirs, files in os.walk("notes/"):
-    for filename in files:
-        fname = os.path.join(dirpath,filename)
-        if pattern.search(fname):
-            gen_file(File=fname, link=filename)
-            gen_link(File=fname, link=filename)
-
 def gen_link(File, link):
     topic = extract_topic(File)
     print(topic)
@@ -27,3 +18,26 @@ def extract_topic(File):
             break
         topic += c
     return topic
+
+def gen_notes():
+    pass
+
+topic_map = {}
+pattern = re.compile(".tex")
+for dirpath, dirs, files in os.walk("notes/"):
+    for filename in files:
+        fname = os.path.join(dirpath,filename)
+        if pattern.search(fname):
+            gen_file(File=fname, link=filename)
+            topic = extract_topic(fname)
+            if topic in topic_map:
+                topic_map[topic] += (fname, filename)
+            else:
+                topic_map[topic] = (fname, filename)
+
+for key, value in topic_map.items():
+    print(key, value)
+
+with open("notes/notes.html", 'w+') as note:
+    with open("notes/template.html", 'r+') as temp:
+
