8c162300f1776180ab06b88db7152613d68c685e
Alex Kazorian <akazorian@berkeley.edu>
2018-01-09 00:00:14 -0800

    I think this works! Going to merge this into master and test.

--- build.py
+++ build.py
@@ -5,6 +5,10 @@ import os
 import re
 from gen_file import *
 
+topic_header = "<div class=\"col-sm-6\">\n<h1>{}:</h1>\n<ul class=\"nav flex-column\">\n"
+topic_close = "</ul>\n</div>\n"
+link_template = "<li class=\"nav-item\"><a href={}>{}</a></li>"
+
 def gen_link(File, link):
     topic = extract_topic(File)
     print(topic)
@@ -30,14 +34,19 @@ for dirpath, dirs, files in os.walk("notes/"):
         if pattern.search(fname):
             gen_file(File=fname, link=filename)
             topic = extract_topic(fname)
-            if topic in topic_map:
-                topic_map[topic] += (fname, filename)
-            else:
-                topic_map[topic] = (fname, filename)
+            if topic not in topic_map:
+                topic_map[topic] = []
+            topic_map[topic].append((fname.replace(".tex", ".html"), filename.replace(".tex", ".html")))
 
-for key, value in topic_map.items():
-    print(key, value)
-
+start_flag = re.compile("append here")
 with open("notes/notes.html", 'w+') as note:
     with open("notes/template.html", 'r+') as temp:
-
+        for line in temp:
+            note.write(line)
+            if start_flag.search(line):
+                for key, value in topic_map.items():
+                    note.write(topic_header.format(key))
+                    for pair in value:
+                        file_name, link = pair
+                        note.write(link_template.format(file_name, link))
+                    note.write(topic_close)
